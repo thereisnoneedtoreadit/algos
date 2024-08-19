e1f6a95a02e9a372fe0ebc2d1430d4925591c8e9
aleksandr.sh <aleksandr.shashnin@ringcentral.com>
2024-08-19 17:46:27 +0400

    linkedlist: delete duplicates

--- linkedlist/main.go
+++ linkedlist/main.go
@@ -1,25 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
-
-//TIP To run your code, right-click the code and select <b>Run</b>. Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.
-
+/*
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+*/
 func main() {
-	//TIP Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined or highlighted text
-	// to see how GoLand suggests fixing it.
-	s := "gopher"
-	fmt.Println("Hello and welcome, %s!", s)
+	n3 := &ListNode{Val: 1, Next: nil}
+	n2 := &ListNode{Val: 1, Next: n3}
+	n1 := &ListNode{Val: 2, Next: n2}
 
-	for i := 1; i <= 5; i++ {
-		//TIP You can try debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>. To start your debugging session,
-		// right-click your code in the editor and select the <b>Debug</b> option.
-		fmt.Println("i =", 100/i)
+	head := deleteDuplicates(n1)
+
+	for head != nil {
+		print(head.Val, " ")
+		head = head.Next
 	}
 }
 
-//TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
-// Also, you can try interactive lessons for GoLand by selecting 'Help | Learn IDE Features' from the main menu.
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	current := head
+	for current != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+	return head
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
