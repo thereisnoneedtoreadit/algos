64d7d5773cc24b752fa99e28cffe46dc9a7bac1c
alexsh <work.aleksandr.sh@gmail.com>
2024-10-20 20:26:18 +0600

    simplify path

---
+++ stacks_and_queues/simplify_path/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/*
+You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+
+The rules of a Unix-style file system are as follows:
+
+A single period '.' represents the current directory.
+A double period '..' represents the previous/parent directory.
+Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
+Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+The simplified canonical path should follow these rules:
+
+The path must start with a single slash '/'.
+Directories within the path must be separated by exactly one slash '/'.
+The path must not end with a slash '/', unless it is the root directory.
+The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
+Return the simplified canonical path.
+*/
+func main() {
+	path := "/home//foo/"
+	fmt.Println(simplifyPath(path))
+}
+
+func simplifyPath(path string) string {
+	var stack []string
+
+	for _, dir := range strings.Split(path, "/") {
+		switch dir {
+		case "", ".":
+			continue
+		case "..":
+			stack = pop(stack)
+		default:
+			stack = push(stack, dir)
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
+}
+
+func pop(stack []string) []string {
+	if len(stack) > 0 {
+		stack = stack[:len(stack)-1]
+	}
+	return stack
+}
+
+func push(stack []string, dir string) []string {
+	return append(stack, dir)
+}
